be99bc28d153e1fc817f2d5deb28071d67b6c644
Colin Knox <colin.r.knox@gmail.com>
2022-02-08 10:19:55 -0600

    Initial spring boot project

---
+++ CashOverflow/src/main/java/com/revature/CashOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashOverflowApplication.class, args);
+	}
+
+}
