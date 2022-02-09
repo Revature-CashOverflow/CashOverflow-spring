e170ae63fa17e7630ef07c613a34b5c8b958d417
esosa001 <sosaeman@gmail.com>
2022-02-09 14:48:07 -0500

    l

--- CashOverflow/src/main/java/com/revature/controller/LoginController.java
+++ CashOverflow/src/main/java/com/revature/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.revature.controller;
 
-import java.util.Map;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
-import org.springframework.web.bind.annotation.RequestBody;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.User;
@@ -13,8 +17,20 @@ public class LoginController {
 
 	LoginService serv;
 	
-	public User login(@RequestBody Map<String, Object> json) {
-		
-	}
+	 @GetMapping("/login")
+	    public User login(@RequestParam ("loginUsername") String username, @RequestParam ("loginPassword") String password) {
+	        System.out.println("username===>" + username);
+	        System.out.println("password===>" + password);
 
-}
+	        User user = serv.login(username, password);
+	        return user;
+	    }
+//	 @GetMapping(value = "/loginUser")
+//		public User loginUser(HttpSession session, @RequestParam("loginUsername") String username,
+//				@RequestParam("loginPassword") String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+//			User user = serv.login(username, passwordHashing(password));
+//			
+//				return new User(null, null, null);
+//			
+//		}
+	}
