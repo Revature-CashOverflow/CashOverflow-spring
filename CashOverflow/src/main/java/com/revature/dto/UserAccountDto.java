032743252f98acbd948afe1212e867ca9b0b1055
esosa001 <sosaeman@gmail.com>
2022-02-10 15:21:12 -0500

    scrummaster changes

---
+++ CashOverflow/src/main/java/com/revature/dto/UserAccountDto.java
@@ -0,0 +1,36 @@
+package com.revature.dto;
+
+import java.time.Instant;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.revature.model.UserAccount;
+
+import lombok.Data;
+
+/**
+ * Model for user accounts
+ * 
+ * @author Colin Knox, Parker Mace, Tyler Rondeau
+ */
+@Data
+public class UserAccountDto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	String username;
+	String firstName;
+	String lastName;
+	Instant creationDate;
+
+	public UserAccountDto(UserAccount user) {
+		this.id = user.getId();
+		this.username = user.getUsername();
+		this.firstName = user.getFirstName();
+		this.lastName = user.getLastName();
+		this.creationDate = user.getCreationDate();
+	}
+}
