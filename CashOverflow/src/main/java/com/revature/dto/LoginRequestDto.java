b20beecdb9c66bf6a4fe4f199ece736e35438d2f
lyly2112 <lyly2112@gmail.com>
2022-02-11 11:48:23 -0800

    Merge with dev.

--- CashOverflow/src/main/java/com/revature/dto/LoginRequestDto.java
+++ CashOverflow/src/main/java/com/revature/dto/LoginRequestDto.java
@@ -1,13 +1,5 @@
 package com.revature.dto;
 
-import java.time.Instant;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import com.revature.model.UserAccount;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
