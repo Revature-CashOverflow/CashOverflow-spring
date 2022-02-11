cff4c0d02d3e0a8213f2ad091e54cbf9363c3d95
cameronaumann <cameron.aumann1@gmail.com>
2022-02-11 14:32:37 -0800

    some testing

--- CashOverflow/src/main/java/com/revature/controller/RegisterController.java
+++ CashOverflow/src/main/java/com/revature/controller/RegisterController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.revature.model.UserAccount;
 import com.revature.service.RegisterService;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @CrossOrigin(value = "http://localhost:4200")
 @Controller
@@ -22,8 +25,10 @@ public class RegisterController {
 	 * @author cam77 + scrummybois
 	 *
 	 */
+	@Getter
+	@NoArgsConstructor
 	@AllArgsConstructor
-	private class UserDto {
+	public static class UserDto {
 		String email;
 		String username;
 		String firstName;
@@ -43,13 +48,16 @@ public class RegisterController {
 	
 	
 	/**
-	 * TODO: explain dto thing, password mismatch, error handling, + controller testing
-	 * 
+	 * TODO: password mismatch, error handling, + controller testing
+	 * In order to guard against SQL injection we have created a data transfer object to act as a buffer 
 	 * @authors Cameron, Amir, Chandra
 	 */
 	@PostMapping("/register")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void newUser(@RequestBody UserDto userDto) {
+		if (userDto.email == null || userDto.username == null || userDto.firstName == null || userDto.lastName == null || userDto.password == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing registration info");
+		}
 		UserAccount user = new UserAccount();
 		user.setEmail(userDto.email);
 		user.setUsername(userDto.username);
@@ -59,6 +67,8 @@ public class RegisterController {
 		regServ.insertUserAccount(user);
 		
 	}
+	
+	
 	
 	
 	
--- CashOverflow/src/test/java/com/revature/RegisterTests.java
+++ CashOverflow/src/test/java/com/revature/RegisterTests.java
@@ -1,6 +1,8 @@
 package com.revature;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +12,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.revature.controller.RegisterController;
 import com.revature.dao.UserAccountRepo;
 import com.revature.model.UserAccount;
+import com.revature.service.RegisterService;
 import com.revature.service.RegisterServiceImpl;
 
 @SpringBootTest
@@ -19,13 +23,21 @@ import com.revature.service.RegisterServiceImpl;
 class RegisterTests {
 
 	private RegisterServiceImpl regServ;
+	private RegisterController regCont;
 
 	@Mock
 	private UserAccountRepo mockRepo;
-
+	
+	@Mock
+	private RegisterService mockServ;
+	
+	
 	@BeforeEach
 	public void setUp() {
 		regServ = new RegisterServiceImpl(mockRepo);
+		regCont = new RegisterController(mockServ);
+		
+		
 	}
 
 	@Test
@@ -36,9 +48,26 @@ class RegisterTests {
 		
 		when(mockRepo.save(test)).thenReturn(test);
 		UserAccount result = regServ.insertUserAccount(test);
-
+		verify(mockRepo, times(1)).save(test);
 		assertNotNull(result.getCreationDate());
 
 	}
+	
+	@Test
+	void controllerTest() {
+		RegisterController.UserDto test = new RegisterController.UserDto("email@gmail.com", "username", "firstname", "lastname", "password");
+		
+		UserAccount user = new UserAccount();
+		user.setEmail(test.getEmail());
+		user.setUsername(test.getUsername());
+		user.setFirstName(test.getFirstName());
+		user.setLastName(test.getLastName());
+		user.setPassword(test.getPassword());
+		
+		verify(regServ, times(1)).insertUserAccount(user);
+		
+	}
+	
+	
 
 }
