c64f6ae9f374cfa9d607336c116e1920983abb59
lyly2112 <lyly2112@gmail.com>
2022-02-11 13:29:35 -0800

    Remove dead code from application test file.

--- CashOverflow/src/test/java/com/revature/CashOverflowApplicationTests.java
+++ CashOverflow/src/test/java/com/revature/CashOverflowApplicationTests.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.revature.controller.LoginController;
-<<<<<<< HEAD
-=======
+
 import com.revature.service.LoginServiceImpl;
->>>>>>> 7f9a884147a16615a4ae2ffab8d6661297c1616e
 
 @SpringBootTest
 class CashOverflowApplicationTests {
