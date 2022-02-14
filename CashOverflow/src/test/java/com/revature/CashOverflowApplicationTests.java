8f13f0a889492116012806c7804d537ed60a1936
lyly2112 <lyly2112@gmail.com>
2022-02-14 12:18:49 -0800

    Remove servers folder. Update test file for application.

--- CashOverflow/src/test/java/com/revature/CashOverflowApplicationTests.java
+++ CashOverflow/src/test/java/com/revature/CashOverflowApplicationTests.java
@@ -1,34 +1,14 @@
 package com.revature;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.revature.controller.LoginController;
-
-import com.revature.service.LoginServiceImpl;
 
 @SpringBootTest
 class CashOverflowApplicationTests {
-	
-	@Autowired
-	private LoginController loginController;
-	
-	@Autowired
-	private LoginServiceImpl loginServiceImpl;
 
 	@Test
 	void contextLoads() {
-		assertThat(loginController).isNotNull();
-		assertThat(loginServiceImpl).isNotNull();
-	}
-	
-	@Test
-	public void contextLoads() throws Exception {
-		assertThat(controller).isNotNull();
+
 	}
 
-	
 }
