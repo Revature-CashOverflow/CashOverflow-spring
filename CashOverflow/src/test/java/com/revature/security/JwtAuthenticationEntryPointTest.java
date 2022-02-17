e5bde856ed46da05400f941c759999dac4dfe046
Luis Rivera <luis_rivera@revature.net>
2022-02-17 13:21:50 -0500

    created tests for the AuthenticationEntryPoint'

--- CashOverflow/src/test/java/com/revature/security/JwtAuthenticationEntryPointTest.java
+++ CashOverflow/src/test/java/com/revature/security/JwtAuthenticationEntryPointTest.java
@@ -1,7 +1,14 @@
 package com.revature.security;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,13 +19,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @ExtendWith(MockitoExtension.class)
 class JwtAuthenticationEntryPointTest {
 
+	private final CloseableHttpClient httpClient = HttpClients.createDefault();
+	
+	JwtAuthenticationEntryPoint enPoint;
+
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
+	//for this test the project must be running locally
 	@Test
-	void test() {
-		
+	void test() throws ClientProtocolException, IOException {
+		HttpGet request = new HttpGet("http://localhost:9001/api/account/getBankAccounts");
+		try (CloseableHttpResponse response = httpClient.execute(request)) {
+
+			System.out.println(response.getEntity().getContent());
+			assertEquals(401, response.getStatusLine().getStatusCode());
+		}
 	}
 
 }
