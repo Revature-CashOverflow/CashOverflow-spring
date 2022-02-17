7629f84bd5cdfff22be21ce3c56e06f1d0364a23
Colin Knox <colin.r.knox@gmail.com>
2022-02-16 19:41:28 -0600

    updated the CORS for s3 cloudfront

--- CashOverflow/src/main/java/com/revature/controller/RegisterController.java
+++ CashOverflow/src/main/java/com/revature/controller/RegisterController.java
@@ -15,7 +15,7 @@ import com.revature.dto.RegUserAccountDto;
 import com.revature.model.UserAccount;
 import com.revature.service.RegisterService;
 
-@CrossOrigin(value = {"http://localhost:4200", "http://d3nlmo2v0fs5mq.cloudfront.net"})
+@CrossOrigin(value = {"http://localhost:4200", "http://dostz94b44kp0.cloudfront.net"})
 @Controller
 public class RegisterController {
 	
