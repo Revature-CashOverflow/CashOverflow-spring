c03274a24005efbd6c5a39b483d134a23d20ce68
Luis Rivera <luis_rivera@revature.net>
2022-02-17 13:30:52 -0500

    commented out test that only works locally also removed repo tests

--- CashOverflow/src/test/java/com/revature/security/JwtAuthenticationEntryPointTest.java
+++ CashOverflow/src/test/java/com/revature/security/JwtAuthenticationEntryPointTest.java
@@ -1,16 +1,6 @@
 package com.revature.security;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,23 +9,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @ExtendWith(MockitoExtension.class)
 class JwtAuthenticationEntryPointTest {
 
-	private final CloseableHttpClient httpClient = HttpClients.createDefault();
-	
+//	private final CloseableHttpClient httpClient = HttpClients.createDefault();
+
 	JwtAuthenticationEntryPoint enPoint;
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
-	//for this test the project must be running locally
-	@Test
-	void test() throws ClientProtocolException, IOException {
-		HttpGet request = new HttpGet("http://localhost:9001/api/account/getBankAccounts");
-		try (CloseableHttpResponse response = httpClient.execute(request)) {
-
-			System.out.println(response.getEntity().getContent());
-			assertEquals(401, response.getStatusLine().getStatusCode());
-		}
-	}
+	// for this test the project must be running locally
+//	@Test
+//	void test() throws ClientProtocolException, IOException {
+//		HttpGet request = new HttpGet("http://localhost:9001/api/account/getBankAccounts");
+//		try (CloseableHttpResponse response = httpClient.execute(request)) {
+//
+//			System.out.println(response.getEntity().getContent());
+//			assertEquals(401, response.getStatusLine().getStatusCode());
+//		}
+//	}
 
 }
