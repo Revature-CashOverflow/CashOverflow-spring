e1f0ac99798462c083ec5c823e6361bd44131249
Colin Knox <colin.r.knox@gmail.com>
2022-02-18 14:55:11 -0600

    initial register controller tests

---
+++ CashOverflow/src/test/java/com/revature/integration/RegisterControllerIntegrationTest.java
@@ -0,0 +1,64 @@
+package com.revature.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.CashOverflowApplication;
+import com.revature.dao.UserAccountRepo;
+import com.revature.dto.LoginRequestDto;
+import com.revature.helper.TestHelper;
+
+/**
+ * Integration tests for the register controller
+ * 
+ * @author Colin Knox
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = CashOverflowApplication.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class RegisterControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Autowired
+	private UserAccountRepo repo;
+
+	@Autowired
+	private PasswordEncoder enc;
+
+	@BeforeEach
+	void setUpEach() {
+		repo.deleteAll();
+	}
+
+	@Test
+	void testGetWrongMethod() throws Exception {
+		mvc.perform(get("/register")).andExpect(status().isMethodNotAllowed());
+	}
+
+	@Test
+	void testNullValue() throws Exception {
+		mvc.perform(post("/login").content(TestHelper.asJsonString(new LoginRequestDto("username", null), mapper))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().is4xxClientError());
+	}
+}
