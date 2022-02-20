63004d8a0a1a5fb654c8aa08533011bc71e22b5a
cameronaumann <cameron.aumann1@gmail.com>
2022-02-19 21:47:09 -0800

    pulled and pushed to match dev

--- CashOverflow/src/main/java/com/revature/controller/IncomeExpenseController.java
+++ CashOverflow/src/main/java/com/revature/controller/IncomeExpenseController.java
@@ -1,11 +1,13 @@
 package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.revature.dto.TransactionDto;
 import com.revature.service.TransactionService;
@@ -24,9 +26,9 @@ public class IncomeExpenseController {
 	@PostMapping("/transaction")
 	public void addTransaction(Authentication auth, @RequestBody TransactionDto dto) {
 		System.out.println("here");
-//		if (dto.getAccountId() == 0 || dto.getAmount() == null || dto.getDescription() == null) {
-//			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing transaction info");
-//		}
+		if (dto.getAccountId() == 0 || dto.getAmount() == null || dto.getDescription() == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing transaction info");
+		}
 
 		tranServ.addTransaction(dto);
 	}
--- CashOverflow/src/main/java/com/revature/controller/RegisterController.java
+++ CashOverflow/src/main/java/com/revature/controller/RegisterController.java
@@ -6,34 +6,30 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.revature.dto.RegUserAccountDto;
 import com.revature.model.UserAccount;
 import com.revature.service.RegisterService;
 
-<<<<<<< HEAD
-@CrossOrigin(origins = { "http://localhost:4200", "http://d3nlmo2v0fs5mq.cloudfront.net" })
-@RestController
-=======
+//<<<<<<< HEAD
+//@CrossOrigin(origins = { "http://localhost:4200", "http://d3nlmo2v0fs5mq.cloudfront.net" })
+//@RestController
+//=======
 @CrossOrigin(value = { "http://localhost:4200", "http://dostz94b44kp0.cloudfront.net" })
 @Controller
->>>>>>> be56820e6ec86425d9c30a814cc797d832615cea
 public class RegisterController {
 
 	private RegisterService regServ;
 	private ModelMapper mapper;
 
 	private PasswordEncoder enc;
-<<<<<<< HEAD
-	
-=======
->>>>>>> be56820e6ec86425d9c30a814cc797d832615cea
+
 
 	@Autowired
 	public RegisterController(RegisterService regServ, ModelMapper mapper, PasswordEncoder enc) {
@@ -56,16 +52,13 @@ public class RegisterController {
 	@PostMapping("/register")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void newUser(@RequestBody RegUserAccountDto dto) {
-<<<<<<< HEAD
-		if (dto.getEmail() == null || dto.getUsername() == null || dto.getFirstName() == null
-				|| dto.getLastName() == null || dto.getPassword() == null) {
-=======
+
 		Pattern ptr = Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");		
 		if (dto.getEmail() == null || dto.getEmail().isBlank() || dto.getUsername() == null
 				|| dto.getUsername().isBlank() || dto.getFirstName() == null || dto.getFirstName().isBlank()
 				|| dto.getLastName() == null || dto.getLastName().isBlank() || dto.getPassword() == null
 				|| dto.getPassword().isBlank() || !ptr.matcher(dto.getEmail()).matches()) {
->>>>>>> be56820e6ec86425d9c30a814cc797d832615cea
+
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing registration info");
 		}
 		dto.setPassword(enc.encode(dto.getPassword()));
