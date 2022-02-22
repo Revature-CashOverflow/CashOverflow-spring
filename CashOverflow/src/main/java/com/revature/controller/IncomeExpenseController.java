23d20b60a9ffb24c5f2a9ad93e518c648b5fbc2c
Colin Knox <colin.r.knox@gmail.com>
2022-02-22 13:35:46 -0600

    updated cors url for income expense controller

--- CashOverflow/src/main/java/com/revature/controller/IncomeExpenseController.java
+++ CashOverflow/src/main/java/com/revature/controller/IncomeExpenseController.java
@@ -16,7 +16,7 @@ import com.revature.dto.TransactionDto;
 import com.revature.model.Transaction;
 import com.revature.service.TransactionService;
 
-@CrossOrigin(origins = { "http://localhost:4200", "http://d3nlmo2v0fs5mq.cloudfront.net" })
+@CrossOrigin(origins = { "http://localhost:4200", "http://dostz94b44kp0.cloudfront.net/" })
 @RestController
 public class IncomeExpenseController {
 
